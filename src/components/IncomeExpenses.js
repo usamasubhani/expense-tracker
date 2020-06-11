786494a5f60bbcb7bad4d6839e0d22ecdb3ddf1e
Usama Subhani <reachusama@protonmail.com>
2020-06-11 20:50:24 +0500

    Calculate income and expenses

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react';
+
+import { GlobalContext } from '../context/GlobalContext'
 
 export const IncomeExpenses = () => {
+    const { transactions } = useContext(GlobalContext);
+
+    const transactioAmounts = transactions.map(transaction => transaction.amount);
+    const income = transactioAmounts.filter(i => i>0).reduce((a, b) => (a+b), 0)
+    const expense = -transactioAmounts.filter(i => i<0).reduce((a, b) => (a+b), 0)
     return (
         <div>
             <div className="inc-exp-container">
                 <div>
                     <h4>Income</h4>
-                    <p className="money plus">$1000</p>
+                    <p className="money plus">${income}</p>
                 </div>
                 <div>
                     <h4>Expense</h4>
-                    <p className="money minus">$500</p>
+                    <p className="money minus">${expense}</p>
                 </div>
             </div>
         </div>
